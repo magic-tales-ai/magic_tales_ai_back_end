3110fe093292973fdae2eb5ae2fb10e1d0909a66
Munir Jojo-Verge <munirjojoverge@yahoo.es>
2024-04-03 16:05:15 -0400

    fix: add requirements.txt

---
+++ services/openai_assistants/assistant_streaming.py
@@ -0,0 +1,348 @@
+from abc import ABC, abstractmethod
+from typing import Any
+import traceback
+import os
+import asyncio
+import json
+import logging
+import re
+from typing import Dict, List, Optional, Tuple, Callable
+from openai import AsyncOpenAI
+from openai.types.beta.threads import (
+    ThreadMessage,
+    MessageContentText,
+    MessageContentImageFile,
+)
+
+from services.utils.log_utils import get_logger
+from services.openai_assistants.prompt_utils import (
+    async_load_prompt_template_from_file,
+)
+from services.openai_assistants.event_handler import EventHandler
+from models.ws_input import WSInput
+from models.user import User
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = get_logger(__name__)
+
+
+class Assistant(ABC):
+    def __init__(self, config, command_handler: Optional[Callable] = None):
+        """
+        Initialize the OpenAI Assistant.
+
+        Args:
+            config (DictConfig): Configuration parameters.            
+        """
+        self._validate_openai_api_key()
+        self.config = config
+        self.client = AsyncOpenAI()
+        self.openai_assistant = None
+        self.openai_thread = None
+        self.file_ids = []
+        self.command_handler = command_handler             
+        
+
+    def _validate_openai_api_key(self):
+        """Validates the presence of the OpenAI API key."""
+        self.openai_api_key = os.environ.get("OPENAI_API_KEY")
+        if not self.openai_api_key:
+            raise ValueError("OPENAI_API_KEY is not set.")
+
+    async def _create_assistant(self):
+        """
+        Creates a NEW OpenAI assistant with attached knowledge base files.
+
+        """
+        if not self.config.create_instructions_path:
+            raise ValueError("Instructions path is required for assistant creation.")
+        
+        instructions = await async_load_prompt_template_from_file(
+            self.config.create_instructions_path
+        )
+        
+        self.openai_assistant = await self.client.beta.assistants.create(
+            name=self.config.name,
+            instructions=instructions,
+            model=self.config.model,
+            tools=self.config.tools,
+            file_ids=self.file_ids or [],        
+        )
+        self.openai_thread = await self.client.beta.threads.create()
+        logger.info("OpenAI Assistant Created.")
+
+    async def _retrieve_assistant(self, user: User = None):
+        """
+        Retrieves an existing OpenAI assistant with attached knowledge base files.
+
+        """        
+        if not user or not user.assistant_id:
+            logger.warning("Assistant ID is required for retrieval. We will create a new Assistant.")
+            await self._create_assistant()
+            return        
+
+        self.openai_assistant = await self.client.beta.assistants.update(
+            assistant_id=user.assistant_id,            
+            model=self.config.model,
+            tools=self.config.tools,
+            file_ids=self.file_ids or [],    
+        )
+
+        self.openai_thread = await self.client.beta.threads.create()
+        logger.info("OpenAI Assistant Retrieved.")
+
+    async def _update_assistant(self):
+        """
+        Initializes the OpenAI assistant with attached knowledge base files.
+
+        """
+        if not self.openai_assistant or not self.openai_assistant.id:
+            raise ValueError("Assistant ID is required for updating.")
+
+        self.openai_assistant = await self.client.beta.assistants.update(
+            assistant_id=self.openai_assistant.id,
+            file_ids=self.file_ids or [],
+        )
+        logger.info("OpenAI Assistant UPDATED.")
+
+    async def request_ai_response(
+        self, message: str, parsing_method: Optional[Callable] = None
+    ) -> Tuple[str, List]:
+        """
+        Process the incoming message and generate an AI response.
+
+        The function sends the message to the OpenAI assistant and retrieves the response.
+        It handles the entire process including sending the message, initiating the run, waiting
+        for completion, and processing the received response.
+
+        Args:
+            message (str): The message received from the client.
+            parsing_method (Callable, optional): A method to parse the AI response.
+
+        Returns:
+            Tuple[str, List]: A tuple containing the response message for the human and
+                              the raw data of messages.
+
+        Raises:
+            Exception: If the OpenAI API call fails or the parsing method encounters an error.
+        """
+        messages_data = []
+
+        try:
+            # Send the message to the OpenAI assistant
+            await self._send_message_to_assistant(message)
+            # Initiate the assistant run and wait for completion
+            with self.client.beta.threads.runs.create_and_stream(
+                thread_id=self.openai_thread.id,
+                assistant_id=self.openai_assistant.id,               
+                event_handler=EventHandler(self.openai_thread.id, self.openai_assistant.id),
+                ) as stream:
+                stream.until_done()
+
+
+            # run = await self._wait_for_assistant_run_completion()
+
+            # Retrieve the latest messages from the assistant
+            messages_data = await self._retrieve_messages()
+
+            # Extract the latest AI response message
+            ai_message_for_human, ai_message_for_system = (
+                await self._process_ai_response(messages_data, parsing_method)
+            )
+
+            # Handle any commands the system included in the AI response
+            if self.command_handler and ai_message_for_system:
+                await self.command_handler(ai_message_for_system)
+
+            return ai_message_for_human, messages_data
+
+        except Exception as e:
+            logger.error(
+                f"An error occurred during AI response generation: {e}/n/n{traceback.format_exc()}", exc_info=True
+            )
+            return "", messages_data
+
+    async def _send_message_to_assistant(self, message: str) -> None:
+        """
+        Sends the user's message to the OpenAI assistant thread.
+
+        Args:
+            message (str): The message to send to the assistant.
+
+        Raises:
+            Exception: If sending the message fails.
+        """
+        await self.client.beta.threads.messages.create(
+            thread_id=self.openai_thread.id,
+            role="user",
+            content=message,
+        )
+        logger.info(f"Message sent to OpenAI Assistant: {message}")
+
+    async def _wait_for_assistant_run_completion(self) -> Any:
+        """
+        Initiates a run with the OpenAI assistant and waits for it to complete.
+
+        Returns:
+            Any: The run object after completion.
+
+        Raises:
+            Exception: If initiating or waiting for the run fails.
+        """
+        run = await self.client.beta.threads.runs.create(
+            thread_id=self.openai_thread.id,
+            assistant_id=self.openai_assistant.id,
+        )
+        logger.info("Assistant run initiated, waiting for completion...")
+        while run.status in ["queued", "in_progress"]:
+            await asyncio.sleep(0.5)  # Non-blocking sleep
+            run = await self.client.beta.threads.runs.retrieve(
+                thread_id=self.openai_thread.id,
+                run_id=run.id,
+            )
+        logger.info("Assistant run completed.")
+        return run
+
+    async def _retrieve_messages(self) -> List:
+        """
+        Retrieves the list of messages from the OpenAI assistant thread in descending order.
+
+        Returns:
+            List: The list of messages data.
+
+        Raises:
+            Exception: If retrieving the messages fails.
+        """
+        messages = await self.client.beta.threads.messages.list(
+            thread_id=self.openai_thread.id, order="desc"
+        )
+        logger.info("Retrieved messages from OpenAI Assistant.")
+        return messages.data
+
+    async def _process_ai_response(
+        self, messages_data: List, parsing_method: Optional[Callable] = None
+    ) -> Tuple[str, Any]:
+        """
+        Processes the OpenAI assistant's latest response.
+
+        Args:
+            messages_data (List): The list of messages data from the assistant.
+            parsing_method (Callable, optional): A method to parse the AI response.
+
+        Returns:
+            Tuple[str, Any]: A tuple of the AI's response for the human and the system command.
+
+        Raises:
+            Exception: If processing the response fails.
+        """
+        # Process the response to find the latest AI message
+        ai_message = next((m for m in messages_data if m.role == "assistant"), None)
+        if not ai_message:
+            raise Exception("No AI message found.")
+
+        # Ensure that ai_message.content has at least one element
+        if not ai_message.content or len(ai_message.content) == 0:
+            raise Exception("AI message content is empty.")
+
+        ai_message_content = ai_message.content[0].text.value
+        if parsing_method:
+            return parsing_method(ai_message_content)
+        else:
+            return self._default_parsing(ai_message_content)
+    
+    @abstractmethod
+    def _default_parsing(
+        self, ai_message_content: str
+    ) -> Tuple[Optional[str], Optional[WSInput]]:
+        """
+        Robustly parses AI response content, extracting `message_for_human` and creating a WSInput instance for `message_for_system`.
+        """
+        pass
+
+    async def create_and_upload_knowledge_files(
+        self, files_paths: List[str]
+    ) -> List[str]:
+        """
+        Uploads knowledge base files to OpenAI and returns their IDs.
+
+        Args:
+            files_paths (List[str]): Paths to the knowledge base files.
+
+        Returns:
+            List[str]: A list of file IDs after upload.
+        """
+        # Ensure files_paths is a list, even if it's empty
+        files_paths = files_paths or []
+        file_ids = []
+        for file_path in files_paths:
+            try:
+                with open(file_path, "rb") as file:
+                    uploaded_file = await self.client.files.create(
+                        file=file, purpose="assistants"
+                    )
+                    file_ids.append(uploaded_file.id)
+            except Exception as e:
+                logger.error(
+                    f"Could not upload file {file_path} to OpenAI: {e}"
+                )
+                continue  # Skip this file and proceed with the next iteration
+            
+        return file_ids
+
+    async def update_and_upload_knowledge_files(
+        self, files_paths: List[str]
+    ) -> List[str]:
+        """
+        UPDATES the changed files and Uploads knowledge base files to OpenAI and returns their IDs.
+
+        Args:
+            files_paths (List[str]): Paths to the new knowledge base files.
+
+        Returns:
+            List[str]: A list of file IDs after upload.
+        """
+        # First delete the old files
+        for file_id in self.file_ids:
+            try:
+                await self.client.files.delete(file_id)
+            except Exception as e:
+                logger.info(f"Could not delete file with ID {file_id}: {e}")  # Log or print the error
+                continue  # Skip this file and proceed with the next iteration
+        self.file_ids = []
+
+        # Then upload the new files
+        return await self.create_and_upload_knowledge_files(files_paths)
+
+    async def start_assistant(
+        self, user: User = None, files_paths: List[str] = []
+    ):
+        """
+        Starts the chat assistant.
+        """
+        try:
+            self.file_ids = await self.create_and_upload_knowledge_files(files_paths)
+            if user:
+                await self._retrieve_assistant(user)
+            else:
+                await self._create_assistant()
+
+            logger.info("OpenAI Assistant initialized.")
+
+        except Exception as e:
+            logger.error(f"Error initializing assistant: {traceback.format_exc()}")
+            raise e
+
+    async def update_assistant(self, files_paths: List[str]):
+        """
+        Starts the chat assistant.
+        """
+        try:            
+            self.file_ids = await self.update_and_upload_knowledge_files(
+                files_paths
+            )
+            await self._update_assistant()
+
+        except Exception as e:
+            logger.error(f"Error updating assistant: {traceback.format_exc()}")
+            raise e
